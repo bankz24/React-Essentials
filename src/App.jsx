ed0ca197bdd55be3928f1f6150d9f83e50b8d7f5
Idoko Edward <idokoedward24@gmail.com>
2026-02-05 10:19:21 +0100

    Deriving snd outputing data based on state

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import  { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import Coreconcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
-
+import { EXAMPLES } from './data.js';
 
 
 
 
 
 function App() {
-  const [selectedtopic, setselectedtopic] = useState('Please click a button');
+  const [selectedtopic, setselectedtopic] = useState('Components');
 
    function handleSelect(selectedButton){
     //selectedButton =>'Components', jsx,props,states
@@ -38,11 +38,19 @@ function App() {
         <h2>Examples</h2>
         <menu>
           <TabButton onSelect={()=>handleSelect('components')}>Components</TabButton>
-          <TabButton onSelect={()=>handleSelect('JSX')}>JSX</TabButton>
-          <TabButton onSelect={()=>handleSelect('PROPS')}>PROPS</TabButton>
-          <TabButton onSelect={()=>handleSelect('STATE')}>STATE</TabButton>
+          <TabButton onSelect={()=>handleSelect('JSX')}>Jsx</TabButton>
+          <TabButton onSelect={()=>handleSelect('Props')}>Props</TabButton>
+          <TabButton onSelect={()=>handleSelect('State')}>State</TabButton>
         </menu>
-        {selectedtopic}
+        <div id = "tab-content">
+          <h3>{EXAMPLES[selectedtopic].title}</h3>
+          <p>{EXAMPLES[selectedtopic].description}</p>
+          <pre>
+            <code>
+              <code>{EXAMPLES[selectedtopic].code}</code>
+            </code>
+          </pre>
+        </div>
        </section>
      
       <main>
